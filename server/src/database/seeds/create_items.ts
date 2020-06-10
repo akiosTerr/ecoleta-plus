b50aad96955910578c0514c8e68f8fccd9c5018a
akios dev <akiosdev6@gmail.com>
2020-06-10 15:31:34 -0300

    added online URI for images

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -3,7 +3,7 @@ import Knex from 'knex';
 export async function seed(knex: Knex) {
 	await knex('items').insert([
 		{ title: 'lamps', image: 'lamps.svg' },
-		{ title: 'bateries', image: 'bateries.svg' },
+		{ title: 'batteries', image: 'batteries.svg' },
 		{ title: 'paper/cardboard', image: 'paper-cardboard.svg' },
 		{ title: 'electronic waste', image: 'electronics.svg' },
 		{ title: 'Organic', image: 'organic.svg' },
--- server/src/routes/items.ts
+++ server/src/routes/items.ts
@@ -3,14 +3,37 @@ import knex from '../database/connection';
 
 const router = Router();
 
+// '1': 'https://svgshare.com/i/LuU.svg',
+// 	'2': 'https://svgshare.com/i/LwE.svg',
+// 	'3': 'https://svgshare.com/i/LvD.svg',
+// 	'4': 'https://svgshare.com/i/LvZ.svg',
+// 	'5': 'https://svgshare.com/i/LuV.svg',
+// 	'6': 'https://svgshare.com/i/Lvk.svg',
+
+const URIs = {
+	19: 'https://svgshare.com/i/LuU.svg',
+	20: 'https://svgshare.com/i/LwE.svg',
+	21: 'https://svgshare.com/i/LvD.svg',
+	22: 'https://svgshare.com/i/LvZ.svg',
+	23: 'https://svgshare.com/i/LuV.svg',
+	24: 'https://svgshare.com/i/Lvk.svg',
+};
+
+interface Item {
+	id: string;
+	title: string;
+	image: string;
+}
+
 router.get('/', async (req, res) => {
 	const items = await knex('items').select('*');
 
-	const serializedItems = items.map((item) => {
+	const serializedItems = items.map((item: Item) => {
 		return {
 			id: item.id,
 			title: item.title,
 			image_url: `http://localhost:3333/uploads/images/${item.image}`,
+			global_url: URIs[item.id],
 		};
 	});
 
